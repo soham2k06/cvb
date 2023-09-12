2864dede7d8d585ccde7c1b906af683a4c69294a
“soham2k06” <sohmmdev@gmail.com>
2023-09-12 14:03:12 +0530

    toast theme set according to color mode

--- src/App.jsx
+++ src/App.jsx
@@ -60,26 +60,6 @@ function App() {
           </Routes>
         </BrowserRouter>
 
-        {/* <Toaster
-          position="top-center"
-          gutter={12}
-          containerStyle={{ margin: "8px" }}
-          toastOptions={{
-            success: {
-              duration: 3000,
-            },
-            error: {
-              duration: 5000,
-            },
-            style: {
-              fontSize: "16px",
-              maxWidth: "500px",
-              padding: "16px 24px",
-              backgroundColor: "var(--color-grey-0)",
-              color: "var(--color-grey-700)",
-            },
-          }}
-        /> */}
         <ToastContainer
           position="top-right"
           autoClose={5000}
@@ -90,7 +70,7 @@ function App() {
           pauseOnFocusLoss
           draggable
           pauseOnHover
-          theme="light"
+          theme={localStorage.getItem("isDarkMode") ? "dark" : "light"}
         />
       </QueryClientProvider>
     </DarkModeProvider>
